a71f62603620470d76919361ff5c48af975ec27d
Dilfa Risna <dilfarisna@gmail.com>
2025-08-11 17:38:56 +0000

    Buatkan card carausel lebih lebih lebar sedikit lagi

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -270,17 +270,17 @@ export default function HomePageContent() {
               onMouseLeave={plugin.current.reset}
             >
               <CarouselContent className="-ml-2">
-                <CarouselItem className="basis-4/5 md:basis-1/2 pl-2">
+                <CarouselItem className="basis-[90%] md:basis-1/2 pl-2">
                   <div className="p-1 h-36">
                     <DailyQuizCard />
                   </div>
                 </CarouselItem>
-                <CarouselItem className="basis-4/5 md:basis-1/2 pl-2">
+                <CarouselItem className="basis-[90%] md:basis-1/2 pl-2">
                   <div className="p-1 h-36">
                   <LatihanSoalCard />
                   </div>
                 </CarouselItem>
-                <CarouselItem className="basis-4/5 md:basis-1/2 pl-2">
+                <CarouselItem className="basis-[90%] md:basis-1/2 pl-2">
                   <div className="p-1 h-36">
                     <DailyQuizCard />
                   </div>

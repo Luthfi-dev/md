3c9ba3654836cb4ba8b091f9b96e1aed8c408832
Dilfa Risna <dilfarisna@gmail.com>
2025-08-21 18:18:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
-import { getArticle } from "../admin/cms/articles/editor/actions";
+import { getArticle } from "@/app/admin/cms/articles/editor/actions";
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import Link from 'next/link';

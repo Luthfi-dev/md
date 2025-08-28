958a12460dff206e32d9728a91f79047a5c4e219
Dilfa Risna <dilfarisna@gmail.com>
2025-08-28 05:43:07 +0000

    Ada yg salah saat di build eror tidak berhasil coba lihat keterangan di

--- src/app/spa/app-settings/page.tsx
+++ src/app/spa/app-settings/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { useToast } from "@/hooks/use-toast';
+import { useToast } from "@/hooks/use-toast";
 import { Save, Loader2, AppWindow, Upload } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { saveAppSettings, type AppMetadata } from './actions';

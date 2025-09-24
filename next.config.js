0027117371a9edd49fe768afd855f3e7d8b8c244
Dilfa Risna <dilfarisna@gmail.com>
2025-09-24 18:28:34 +0000

    Ini ada masalah serius saat di Dev aman api pas sudah di build dan jalan

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -18,6 +20,20 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET_USER: process.env.REFRESH_TOKEN_SECRET_USER,
+    REFRESH_TOKEN_SECRET_ADMIN: process.env.REFRESH_TOKEN_SECRET_ADMIN,
+    REFRESH_TOKEN_SECRET_SUPERADMIN: process.env.REFRESH_TOKEN_SECRET_SUPERADMIN,
+    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    APP_URL: process.env.APP_URL,
+  }
 };
 
 module.exports = nextConfig;
